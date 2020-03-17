5d7a70e1f42628bd8dcfcb20f3b1262a90d5d6e6
Mangi Lal <mangi_lal@persistent.co.in>
2020-03-17 13:59:21 +0530

    Fix for broken build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi All
+          Hi All Mangi This Side
         </p>
         <a
           className="App-link"
